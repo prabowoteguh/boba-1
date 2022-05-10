354812a1b3256c099fe9baee0e227db748473d0c
prabowoteguh <teguhagungprabowo@gmail.com>
2022-05-11 00:01:58 +0700

    pertemuan 7

---
+++ pertemuan7/script.js
@@ -0,0 +1,62 @@
+// cara membuat variable
+// tipe variable 'nama_var' = 'value'
+
+
+// var 
+let y = 20
+
+console.log("y awal : " + y)
+
+y = 50
+console.log("y kedua : " + y)
+
+const phi = 3.14
+console.log(phi)
+
+// const tidak bisa di ubah
+// let bisa berubah
+
+// number
+let hasil = 0
+let bilangan1 = 9
+let bilangan2 = 10
+
+// string
+let kataKerja = "diam"
+
+// boolean
+let apakahLapar = true
+
+if(apakahLapar) {
+    kataKerja = "Makan"
+}
+
+console.log(kataKerja)
+
+document.getElementById('root').innerHTML = kataKerja
+
+
+// mod = selisih dari hasil bagi
+
+// KuKaBaTaKu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
