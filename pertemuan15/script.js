b09dba48653ad2cc557c937ad35dc348d09c31a4
prabowoteguh <teguhagungprabowo@gmail.com>
2022-06-07 16:38:38 +0700

    pertemuan 15 - DOM

---
+++ pertemuan15/script.js
@@ -0,0 +1,177 @@
+// // const title = document.getElementById('title')
+
+// // const p = document.getElementsByTagName('p')
+// // console.log(p)
+
+// // const p2 = document.getElementsByClassName('p2')[0]
+// // console.log(p2)
+// // manipulate
+// // title.style.backgroundColor = 'red'
+// // title.innerHTML = "BOBA"
+// // title.remove()
+
+// // p[0].style.backgroundColor = "lightblue"
+// // p[1].style.backgroundColor = "lightblue"
+
+// // for(let i = 0; i < p.length; i++) {
+// //     p[i].style.backgroundColor = "red"
+// // }
+
+// // p[2].innerHTML = "ini paragraph 3"
+// // p[2].style.fontSize = "50px"
+// // p[2].style.color = "purple"
+
+// // p2.style.backgroundColor = "grey"
+
+// // querySelector
+// // querySelectorAll
+// const list2 = document.querySelector("#b ol li:nth-child(2)")
+// const list = document.querySelectorAll("#b ol li")
+// const paragraph = document.querySelectorAll("#b p")
+
+// console.log(paragraph)
+// // list2.style.fontSize = "30px"
+// // list2.remove()
+
+// // for(let y = 0; y < list.length; y++) {
+// //     list[y].style.backgroundColor = "aqua"
+// // }
+
+// // for(let i = 0; i < paragraph.length; i++) {
+// //     paragraph[i].style.backgroundColor = "red"
+// // }
+
+// const p3 = document.getElementsByClassName('p3')[0]
+
+// p3.setAttribute('name', "paragraph_3")
+// p3.classList.add("warna_merah")
+
+// const hapus = () => {
+//     const lastList = document.querySelector('#b ol li:last-child')
+
+//     console.log(lastList)
+//     lastList.remove()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const h1 = document.getElementById('title')
+// const p = document.getElementsByTagName('p')
+// const p2 = document.getElementsByClassName('p2')[0]
+
+
+// console.log(p2)
+
+
+// h1.style.color = 'red'
+// h1.style.backgroundColor = 'white'
+
+
+// p.style.backgroundColor = "white" 
+// p[1].style.backgroundColor = "white"
+// p[2].style.backgroundColor = "white"
+
+// for (let i = 0; i < p.length; i++) {
+//     p[i].style.backgroundColor = "white"
+// }
+
+
+// p2.style.backgroundColor = "green"
+
+
+
+
+
+
+
+
+// JIKA TIDAK HUJAN, MAKA PERGI MAIN
+
+// OPERATOR
+
+// - ARITMATIKA (+ - / * % ())
+// - CONCATINATION
+
+// - condition
+//   - SAMA DENGAN (==)
+//   - LEBIH BESAR DARI (>)
+//   - LEBIH BESAR DARI SAMA DENGAN (>=)
+//   - LEBIH KURANG DARI (<)
+//   - LEBIH KURANG DARI SAMA DENGAN (<=)
+//   - TIDAK SAMA DENGAN (!=)
+
+// - LOGIKA
+//  - ATAU (||)
+//  - DAN (&&)
+//  - NEGASI (!)
+
+
+
+let cuaca = "hujan";
+function main() {
+    //ngopi
+    //nonton
+}
+
+
+
+if (cuaca == "cerah") {
+    main();
+}
+
+
+let uangTeguh = 50000;
+let uangDiani = 100000;
+
+// jika uang teguh lebih besar dari uang diani, maka tampilkan pesan uang teguh lebih banyak
+
+if (uangTeguh > uangDiani) {
+    console.log("uang teguh lebih banyak")
+} else {
+   console.log("uang diani lebih banyak")
+}
+
+
+let busway = ["1A", "1B", "1C", "6A"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
